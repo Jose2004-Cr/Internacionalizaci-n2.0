201669748497ad35b8ba22e347c761dbae60a491
Johan Valencia <johan.valencia.20@hotmail.com>
2024-07-18 08:04:42 -0500

    Change my works

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,12 @@
     </x-sider>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     @vite (['resources/css/home.css','resources/js/home.js'])
+
     <body>
         <div class="container">
             <div class="flex items-center justify-between mb-6">
                 <h1 class="text-3xl font-bold text-gray-800">Agregados Reciente...</h1>
-                <button id="addEventButton"
-                    class="px-4 py-2 text-white bg-blue-900 rounded shadow-md hover:bg-[#0F293E]">Agregar Evento
+                <button id="addEventButton" class="px-4 py-2 text-white bg-blue-900 rounded shadow-md hover:bg-[#0F293E]">Agregar Evento
                     +</button>
             </div>
             <section>
@@ -19,21 +19,17 @@
                     <div class="mt-12">
                         <h2 class="text-xl font-bold text-gray-800">Eventos Registrados</h2>
                         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                            <div
-                                class="flex flex-wrap items-center justify-between pb-4 space-y-4 flex-column sm:flex-row sm:space-y-0">
+                            <div class="flex flex-wrap items-center justify-between pb-4 space-y-4 flex-column sm:flex-row sm:space-y-0">
                                 <div class="relative">
                                     <div class="absolute inset-y-0 left-0 flex items-center px-3 pointer-events-none">
-                                        <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                stroke-width="2" d="m19 19-4-4m2-7A8 8 0 1 1 1 8a8 8 0 0 1 16 0Z" />
+                                        <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m2-7A8 8 0 1 1 1 8a8 8 0 0 1 16 0Z" />
                                         </svg>
                                     </div>
-                                    <input type="text" id="table-search"
-                                        class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        placeholder="Buscar">
+                                    <input type="text" id="table-search" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Buscar">
                                 </div>
                             </div>
+
                             <table id="eventsTable" class="w-full overflow-y-auto text-sm text-left text-gray-500 dark:text-gray-400">
                                 <thead class="text-xs text-white uppercase bg-gray-700 table-header">
                                     <tr>
@@ -47,8 +43,7 @@
                                 </thead>
                                 <tbody id="eventsTableBody" class="table-body">
                                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                        <td
-                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                             NameEvent</td>
                                         <td class="px-6 py-4">Start Date</td>
                                         <td class="px-6 py-4">End Date</td>
@@ -64,31 +59,28 @@
                 </main>
             </section>
         </div>
+
         <div id="eventModal" class="fixed inset-0 z-50 flex items-center justify-center hidden bg-black bg-opacity-50">
             <div class="w-full max-w-lg p-6 bg-white rounded-lg shadow-lg">
                 <h2 class="mb-4 text-2xl font-bold">Agregar Nuevo Evento</h2>
                 <form id="eventForm">
                     <div class="mb-4">
                         <label for="eventName" class="block mb-2 font-bold text-gray-700">Nombre del Evento</label>
-                        <input type="text" id="eventName"
-                            class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200">
+                        <input type="text" id="eventName" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200">
                     </div>
                     <div class="mb-4">
                         <label for="eventStartDate" class="block mb-2 font-bold text-gray-700">Fecha de Inicio</label>
-                        <input type="date" id="eventStartDate"
-                            class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200">
+                        <input type="date" id="eventStartDate" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200">
                     </div>
                     <div class="mb-4">
                         <label for="eventEndDate" class="block mb-2 font-bold text-gray-700">Fecha de
                             Finalización</label>
-                        <input type="date" id="eventEndDate"
-                            class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200">
+                        <input type="date" id="eventEndDate" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200">
                     </div>
                     <div class="mb-4">
                         <label for="eventLocation" class="block mb-2 font-bold text-gray-700">Ubicación del
                             Evento</label>
-                        <select id="eventLocation"
-                            class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200">
+                        <select id="eventLocation" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200">
                             <option value="">Seleccione un país</option>
                             <option value="Afghanistan">Afghanistan</option>
                             <option value="Albania">Albania</option>
@@ -292,14 +284,11 @@
                     <div class="mb-4">
                         <label for="eventDescription" class="block mb-2 font-bold text-gray-700">Descripción del
                             Evento</label>
-                        <textarea id="eventDescription"
-                            class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200"></textarea>
+                        <textarea id="eventDescription" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200"></textarea>
                     </div>
                     <div class="flex justify-end">
-                        <button type="button" id="closeModalButton"
-                            class="px-4 py-2 mr-2 text-gray-700 bg-gray-200 rounded hover:bg-gray-300">Cancelar</button>
-                        <button type="submit"
-                            class="px-4 py-2 text-white bg-blue-900  rounded hover:bg-[#0F293E]">Agregar Evento</button>
+                        <button type="button" id="closeModalButton" class="px-4 py-2 mr-2 text-gray-700 bg-gray-200 rounded hover:bg-gray-300">Cancelar</button>
+                        <button type="submit" class="px-4 py-2 text-white bg-blue-900  rounded hover:bg-[#0F293E]">Agregar Evento</button>
                     </div>
                 </form>
             </div>
